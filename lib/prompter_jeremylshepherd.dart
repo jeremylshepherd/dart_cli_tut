2fbe2d74b19e54f4b3ebd56c01cfc25e00cc5997
Jeremy L. Shepherd <jeremy.shepherd@lexisnexis.com>
2022-03-15 17:11:11 -0400

    Complete through 382

---
+++ lib/prompter_jeremylshepherd.dart
@@ -0,0 +1,2 @@
+export 'package:prompter_jeremylshepherd/src/option.dart';
+export 'package:prompter_jeremylshepherd/src/prompter.dart';
--- lib/src/prompter.dart
+++ lib/src/prompter.dart
@@ -4,24 +4,30 @@ import 'terminal.dart';
 final Terminal _terminal = const Terminal();
 
 class Prompter {
-  bool askBinary(String prompt) {
-    _terminal.clearScreen();
-    _terminal.printPrompt('$prompt (y/n)');
-    final input = _terminal.collectInput();
-    return input.contains('y');
-  }
-
-  ask(String prompt, List<Option> options) {
+  String _ask(String prompt, List<Option> options) {
     _terminal.clearScreen();
     _terminal.printPrompt(prompt);
     _terminal.printOptions(options);
 
-    final input = _terminal.collectInput();
+    return _terminal.collectInput();
+  }
+
+  /// Asks an Either/Or prompt and returns a boolean value.
+  /// Takes a String param `prompt`.
+  bool askBinary(String prompt) {
+    final input = _ask('$prompt (y/n)', []);
+    return input.contains('y');
+  }
+
+  /// Multiple choice prompt.
+  /// Takes a String param `prompt` and a param `List<String> options`.
+  askMultiple(String prompt, List<Option> options) {
+    final input = _ask(prompt, options);
 
     try {
       return options[int.parse(input)].value;
     } catch (e) {
-      return ask(prompt, options);
+      return _ask(prompt, options);
     }
   }
 }
